76341688b531ba6c45f9667863822082bbe7832f
Mary Po <pnmry@yandex.ru>
2023-12-13 21:17:24 +0300

    Исправлен тест корзины - пока не рабочий

--- pages/shop_page.py
+++ pages/shop_page.py
@@ -2,13 +2,24 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import ShopPageLocators
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import requests
 import time
 
 class ShopPage(BasePage):
-    def click(self):
-        sidebar_name = self.is_visible(*MainPageLocators.PRODUCTS_POPUPS)
+    def log_in(self):
+        login_form = self.is_visible(*ShopPageLocators.LOGIN_FORM)
+        assert login_form, 'Can not make order'
+
+    def go_to_make_order(self):
+        button = self.is_clickable(*ShopPageLocators.MAKE_ORDER_BUTTON)
+        button.click()
+        
+    def should_be_cart(self, product_name):
+        cart = self.is_visible(*ShopPageLocators.CART_FORM)
+        product = self.is_visible(*ShopPageLocators.PRODUCT_NAME)
+        assert product.text == product_name, 'Product not correct'
+    
     
